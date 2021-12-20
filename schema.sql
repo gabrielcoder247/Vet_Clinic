b60bef077be52647932c3f19445cd5e1b6bf07b9
gabrielcoder247 <gabrielcoder247@gmail.com>
2021-12-20 19:24:13 +0100

    Add create animals to schema.sql

--- schema.sql
+++ schema.sql
@@ -1,5 +1,12 @@
 /* Database schema to keep the structure of entire database. */
 
+
 CREATE TABLE animals (
-    name varchar(100)
+    id integer PRIMARY KEY,
+	name text,
+	date_of_birth date,
+	escape_attempts integer,
+	neutered boolean,
+	weight_kg decimal
 );
+
